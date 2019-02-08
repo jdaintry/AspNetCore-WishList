c160887abe6911f8a84931780a5637c03bf4f4c8
jdaintry <47262749+jdaintry@users.noreply.github.com>
2019-02-08 22:13:58 +1100

    Completed the module

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -22,6 +22,31 @@ namespace WishList.Controllers
             return View("Index", model);
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Models.Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
 
     }
 }
