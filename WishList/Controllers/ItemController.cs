47d83db3607c14c6a4f24ee7199b213803b25f36
jdaintry <47262749+jdaintry@users.noreply.github.com>
2019-02-08 21:36:42 +1100

    ciompleted the module

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -9,13 +9,12 @@ namespace WishList.Controllers
 {
     public class ItemController : Controller
     {
-
         private readonly ApplicationDbContext _context;
-
         public ItemController(ApplicationDbContext context)
-        {
-            _context = context;
+        { 
+        _context = context;
         }
+
         public IActionResult Index()
         {
             var model = _context.Items.ToList();
